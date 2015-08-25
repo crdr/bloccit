9421ba3b18266f4b9ac8b70cc3a98fc919861e66
cmike <carmikerdreid@gmail.com>
2015-08-25 21:09:25 +0000

    InsomniaNoir hates git sometimes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+  resources :posts
+
 
   get 'welcome/contact'
 
-  get 'welcome/index'
-
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
   
   get 'welcome/contact'
   root to: 'welcome#index'
